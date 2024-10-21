c078cb919745dd5cd4a1cecd35c467d1b4d0e26f
Shaun Liew Khye Yang <161272658+MrAFirm@users.noreply.github.com>
2024-10-21 21:05:01 +0800

    Re-uploading ft_printf files for Minitalk.
    Uploaded ft_printf and co. because ft_printf is utilised to print out text.

---
+++ ft_printf/testbitwise.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   testbitwise.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkhye-ya <lkhye-ya@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/03 15:49:37 by lkhye-ya          #+#    #+#             */
+/*   Updated: 2024/10/09 14:55:46 by lkhye-ya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// #include "ft_printf.h"
+
+// int main()
+// {
+// 	unsigned int	a;
+// 	unsigned int	b;
+
+// 	/*
+// 	5 is 00000101 in 8-bit binary.
+// 	9 is 00001001 in 8-bit binary.
+// 	*/
+// 	a = 5;
+// 	b = 9;
+// 	ft_printf("a = %u, b = %u\n", a, b);
+// 	ft_printf("a & b = %u\n",a & b); //check if both int is the same
+// 	ft_printf("a | b = %u\n", a | b); //check if one of the int matches
+// 	ft_printf("a ^ b = %u\n", a ^ b); //check if both int are different
+// 	ft_printf("~a = %u\n", a = ~a); //no.
+// 	ft_printf("b << 1 = %u\n", b << 1); //shift bits to the left by one.
+// 	ft_printf("b >> 1 = %u\n", b >> 1); //shift bits to the right by one.
+// }
