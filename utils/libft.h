62f7c90ad8427d8eea5f3568ae716c94e09222b1
Shaun Liew Khye Yang <161272658+MrAFirm@users.noreply.github.com>
2024-10-21 21:12:43 +0800

    Added ft_atoi.c, libft.h and makefile.
    I don't see the need of porting the entire libft's files as Minitalk only needed ft_atoi.c, so i ported the header file, atoi.c file and the makefile respectfully.

---
+++ utils/libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkhye-ya <lkhye-ya@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/12 02:02:17 by sbin-jef          #+#    #+#             */
+/*   Updated: 2024/10/21 21:10:44 by lkhye-ya         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+# include <string.h>
+
+int				ft_atoi(const char *str);
+
+#endif
